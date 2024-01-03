98bb249feced4e7deda126aa299da651796905bb
Ritik-Jaiswal-179 <124776083+Ritik-Jaiswal-179@users.noreply.github.com>
2024-01-03 22:28:36 +0530

    updated

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose")
 
-// const mongooseUrl = "mongodb://127.0.0.1:27017/inotebook"
-const mongooseUrl = "mongodb+srv://ritikjaiswal17904:ritikjaiswal17904@cluster0.0gvplhb.mongodb.net/MERN-TODO"
+const mongooseUrl = "mongodb://127.0.0.1:27017/inotebook"
 
 const connectToMongo = ()=>{
     mongoose.connect(mongooseUrl, { useNewUrlParser: true })
